045d09e59dce3028f7922febdba451986cf79280
Brad Holland <bholland.mail@gmail.com>
2014-07-12 09:46:44 -0500

    imagebutton gets bitmap from gallery

--- Reading Helper/src/main/java/com/brad/readinghelper/BookDetail.java
+++ Reading Helper/src/main/java/com/brad/readinghelper/BookDetail.java
@@ -2,26 +2,20 @@ package com.brad.readinghelper;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 
 public class BookDetail extends Activity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 1;
+    //static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int IMAGE_PICKER_SELECT = 1;
     ImageButton mBookImageButton;
 
     @Override
@@ -36,64 +30,35 @@ public class BookDetail extends Activity {
             @Override
             public void onClick(View v) {
                 // capture picture
-                dispatchTakePictureIntent();
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, IMAGE_PICKER_SELECT);
             }
         });
     }
 
 
-    public void dispatchTakePictureIntent() {
 
 
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
+    /** * Photo Selection result */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        if(requestCode == IMAGE_PICKER_SELECT && data != null && data.getData() != null) {
+            Uri _uri = data.getData();
 
-            //throws null pointer here after taking photo
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mBookImageButton.setImageBitmap(imageBitmap);
+            //User had pick an image.
+            Cursor cursor = getContentResolver().query(_uri, new String[] { android.provider.MediaStore.Images.ImageColumns.DATA }, null, null, null);
+            cursor.moveToFirst();
+
+            //Link to the image
+            final String imageFilePath = cursor.getString(0);
+            cursor.close();
+            ImageButton mBookImageButton = (ImageButton) findViewById(R.id.imageButton);
+            mBookImageButton.setImageBitmap(BitmapFactory.decodeFile(imageFilePath));
+
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    String mCurrentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
 
 
 
