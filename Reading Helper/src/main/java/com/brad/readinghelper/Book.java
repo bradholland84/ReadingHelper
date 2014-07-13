f5b6ac3230332a01cf016058baa00390ace7c200
Brad Holland <bholland.mail@gmail.com>
2014-07-13 10:34:12 -0500

    working with GSON for internal file storage solutions

--- Reading Helper/src/main/java/com/brad/readinghelper/Book.java
+++ Reading Helper/src/main/java/com/brad/readinghelper/Book.java
@@ -1,34 +1,37 @@
 package com.brad.readinghelper;
 
-import android.os.Parcelable;
-
-import org.parceler.Parcel;
-import org.parceler.Parcels;
-
 /**
  * Created by Brad on 7/12/2014.
  */
 
-@Parcel
+//@Parcel
 public class Book {
 
     String Title;
     String Description;
     float Rating;
+    int Time;
+
+    public Book() {
+        Title = "sample book";
+        Description = "sample description of book that may or may not take up a lot of space";
+        Rating = 3;
+        Time = 40;
+    }
 
 
-
-    public Book (String Title, String Description, float Rating) {
+    public Book(String Title, String Description, float Rating, int Time) {
 
         this.Title = Title;
         this.Description = Description;
         this.Rating = Rating;
+        this.Time = Time;
 
     }
 
-    Parcelable wrappedBook = Parcels.wrap(new Book("Andy", "This is a Test", 4));
+    //Parcelable wrappedBook = Parcels.wrap(new Book("Andy", "This is a Test", 4, 35));
 
-    Book exampleBook = Parcels.unwrap(wrappedBook);
+   // Book testbook = Parcels.unwrap(wrappedBook);
 
     public String getTitle() {
         return Title;
@@ -53,4 +56,13 @@ public class Book {
     public void setRating(float rating) {
         Rating = rating;
     }
+
+    public int getTime() {
+        return Time;
+    }
+
+    public void setTime(int time) {
+        Time = time;
+    }
+
 }
--- Reading Helper/src/main/java/com/brad/readinghelper/BookCard.java
+++ Reading Helper/src/main/java/com/brad/readinghelper/BookCard.java
@@ -9,8 +9,6 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.parceler.Parcel;
-
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
 import it.gmariotti.cardslib.library.internal.CardThumbnail;
@@ -19,14 +17,11 @@ import it.gmariotti.cardslib.library.internal.base.BaseCard;
 /**
  * Created by Brad on 6/7/2014.
  */
-@Parcel
-public class BookCard extends Card  {
+//@Parcel
+public class BookCard extends Card {
 
 
-    private String mBookTitle = "sample title";
-    private String mBookDescription = "This is a sample description that may or may not take up a lot of space";
-    private float mBookRating = (float) 3.5;
-    private int mTime;
+    Book TestBook = new Book("Sample Title", "This is a different Description", 3, 60);
 
 
     public BookCard(Context context) {
@@ -41,13 +36,16 @@ public class BookCard extends Card  {
 
 
 
+
+
+
     /*
     Function to handle book card construction, including all assets
      */
     private void init() {
         CardHeader header = new CardHeader(getContext());
         header.setButtonOverflowVisible(true);
-        header.setTitle(mBookTitle);
+        header.setTitle(TestBook.getTitle());
         header.setPopupMenu(R.menu.card_menu_main, new CardHeader.OnClickCardHeaderPopupMenuListener() {
             @Override
             public void onMenuItemClick(BaseCard card, MenuItem item) {
@@ -66,17 +64,17 @@ public class BookCard extends Card  {
     public void setupInnerViewElements(ViewGroup parent, View view) {
 
         TextView book_description = (TextView) view.findViewById(R.id.book_description);
-        book_description.setText(mBookDescription);
+        book_description.setText(TestBook.getDescription());
 
         RatingBar book_rating_bar = (RatingBar) view.findViewById(R.id.main_inner_ratingBar);
-        book_rating_bar.setRating(mBookRating);
+        book_rating_bar.setRating(TestBook.getRating());
 
     }
 
 
     class BookCardThumb extends CardThumbnail {
 
-        public BookCardThumb (Context context) {
+        public BookCardThumb(Context context) {
             super(context);
         }
 
@@ -87,15 +85,15 @@ public class BookCard extends Card  {
             //viewImage.getLayoutParams().width = 196;
             //viewImage.getLayoutParams().height = 196;
             if (viewImage != null) {
-                if (parent!=null && parent.getResources()!=null){
-                    DisplayMetrics metrics=parent.getResources().getDisplayMetrics();
+                if (parent != null && parent.getResources() != null) {
+                    DisplayMetrics metrics = parent.getResources().getDisplayMetrics();
 
                     int base = 64;
 
-                    if (metrics!=null){
-                        viewImage.getLayoutParams().width = (int)(base*metrics.density);
-                        viewImage.getLayoutParams().height = (int)(base*metrics.density);
-                    }else{
+                    if (metrics != null) {
+                        viewImage.getLayoutParams().width = (int) (base * metrics.density);
+                        viewImage.getLayoutParams().height = (int) (base * metrics.density);
+                    } else {
                         viewImage.getLayoutParams().width = 196;
                         viewImage.getLayoutParams().height = 196;
                     }
@@ -104,37 +102,7 @@ public class BookCard extends Card  {
 
         }
     }
-
-
-    /*
-    GETTERS AND SETTERS
-    *
-    *
-     */
-
-    public int getmTime() {
-        return mTime;
-    }
-
-    public void setmTime(int mTime) {
-        this.mTime = mTime;
-    }
-
-    public String getmBookDescription() {
-        return mBookDescription;
-    }
-
-    public void setmBookDescription(String mBookDescription) {
-        this.mBookDescription = mBookDescription;
-    }
-
-    public void setmBookTitle(String mBookTitle) {
-        this.mBookTitle = mBookTitle;
-    }
-
-    public String getmBookTitle() {
-        return mBookTitle;
-    }
 }
+
 
 
